f77df5db9b29817dd9351386e4c48494a3567bce
Ali <aliii1984@outlook.com>
2020-07-18 12:38:04 +0430

    Auth Service added

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './_services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -6,7 +7,6 @@ import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
-import { from } from 'rxjs';
 
 @NgModule({
    declarations: [
@@ -19,7 +19,9 @@ import { from } from 'rxjs';
       HttpClientModule,
       FormsModule
    ],
-   providers: [],
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
