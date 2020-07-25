35706e3a9e85e64fbc0f451d22863ba407e05d3b
Ali <aliii1984@outlook.com>
2020-07-25 13:21:52 +0430

    minor

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -48,9 +48,7 @@ namespace DatingApp.API.Controllers
 
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
-        {
-            throw new Exception("We are handling exceptions globally!");
-
+        {        
             var userFromRepo = await _repo.Login(userForLoginDto.UserName.ToLower(), userForLoginDto.Password);
 
             if(userFromRepo == null)
