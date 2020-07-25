98f5b984538c4369f10dd27d233d1c14dc1f060d
Ali <aliii1984@outlook.com>
2020-07-25 09:18:02 +0430

    Some clean ups (removed getting values from value service)

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -8,28 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  values: any;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.getValues();
   }
 
   registerToggle(){
     this.registerMode = true;
     console.log(this.registerMode);
-  }
-
-  getValues() {
-    this.http.get('https://localhost:44352/api/values').subscribe(
-      (response) => {
-        this.values = response;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
   }
 
   cancelRegisterMode(registerMode: boolean){
