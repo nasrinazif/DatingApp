d365cac2227829cd0b7edd414999be98cfc318b4
Ali <aliii1984@outlook.com>
2020-07-28 12:14:08 +0430

    Changes to routes and Auth Guards

--- DatingApp-SPA/src/app/_guards/auth.guard.ts
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -1,14 +1,24 @@
+import { AlertifyService } from './../_services/alertify.service';
+import { AuthService } from './../_services/auth.service';
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService,
+    private router: Router
+  ) {}
+
+  canActivate(): boolean {
+    if (this.authService.loggedIn()){
+      return true;
+    }
+    this.alertify.error('You do not have access to this page!!!');
+    this.router.navigate(['/home']);
+    return false;
   }
 }
