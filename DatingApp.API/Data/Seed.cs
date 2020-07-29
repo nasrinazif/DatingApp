ecd04991fab897d586c4638398e29c49c0401e65
Ali <aliii1984@outlook.com>
2020-07-29 10:35:21 +0430

    Seed data dded to database in program.cs

--- DatingApp.API/Data/Seed.cs
+++ DatingApp.API/Data/Seed.cs
@@ -1,4 +1,6 @@
 ﻿using Data;
+using DatingApp.API.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +12,38 @@ namespace DatingApp.API.Data
     {
         public static void SeedUsers(DataContext context)
         {
+            if (!context.Users.Any())
+            {
+                //We want to read UseSeedData.json file.
+                var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
 
+                //Now we want to convert JSON Objects to User Objects
+                var users = JsonConvert.DeserializeObject<List<User>>(userData);
+
+                //Now we have to loop through and do something with it
+                foreach (var user in users)
+                {
+                    byte[] passwordHash, passwordSalt;
+                    CreatePasswordHash("password", out passwordHash, out passwordSalt);
+
+                    user.PasswordHash = passwordHash;
+                    user.PasswordSalt = passwordSalt;
+                    user.Username = user.Username.ToLower();
+
+                    context.Users.Add(user);
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
