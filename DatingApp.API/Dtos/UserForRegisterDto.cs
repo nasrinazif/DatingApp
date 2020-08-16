c9075c96f67ca660f18bd96923a584ab6a941674
Ali <aliii1984@outlook.com>
2020-08-16 08:17:41 +0430

    UserForRegistrationDto Updated

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.Dtos
@@ -9,5 +11,18 @@ namespace DatingApp.API.Dtos
         [Required]
         [StringLength(8, MinimumLength = 4, ErrorMessage ="Password must be between 4 and 8 characters!")]
         public string Password { get; set; }
+        public string Gender { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
