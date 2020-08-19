1d84d627baddfed5149754a920b4ea40a88ad7ab
Ali <aliii1984@outlook.com>
2020-08-19 14:23:04 +0430

    age filter added to API

--- DatingApp.API/Helper/UserParams.cs
+++ DatingApp.API/Helper/UserParams.cs
@@ -18,8 +18,9 @@ namespace DatingApp.API.Helper
 		}
 
 		public int UserId { get; set; }
-
 		public string Gender { get; set; }
+		public int MinAge { get; set; } = 18;
+		public int MaxAge { get; set; } = 99;
 
 	}
 }
