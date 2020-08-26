7c9e239fb95f7333c5915d8ccd2b9b252356b17f
Ali <aliii1984@outlook.com>
2020-08-26 12:17:07 +0430

    Lists Resolver Added

---
+++ DatingApp-SPA/src/app/_resolvers/lists.resolver.ts
@@ -0,0 +1,28 @@
+import { catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { AlertifyService } from '../_services/alertify.service';
+import { UserService } from '../_services/user.service';
+import { Injectable } from '@angular/core';
+import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { User } from '../_models/user';
+
+@Injectable()
+export class ListsResolver implements Resolve<User[]> {
+    pageNumber = 1;
+    pageSize = 6;
+    likesParam = 'Likers';
+
+    constructor(private userService: UserService,
+                private route: Router,
+                private alertify: AlertifyService){}
+
+    resolve(route: ActivatedRouteSnapshot): Observable<User[]>{
+        return this.userService.getUsers(this.pageNumber, this.pageSize, null, this.likesParam).pipe(
+            catchError(error => {
+                this.alertify.error('Problem Retriving Data!');
+                this.route.navigate(['/home']);
+                return of(null);
+            })
+        );
+    }
+}
