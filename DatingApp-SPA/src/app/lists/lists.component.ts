68f4a87b4daa21d9f1219aa0837bb30e21551ed4
Ali <aliii1984@outlook.com>
2020-08-26 12:36:09 +0430

    Lists comopnent updated for likers and likees

--- DatingApp-SPA/src/app/lists/lists.component.ts
+++ DatingApp-SPA/src/app/lists/lists.component.ts
@@ -1,4 +1,11 @@
+import { AuthService } from './../_services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+import { Pagination, PaginatedResult } from '../_models/pagination';
+import { PageChangedEvent } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-lists',
@@ -6,10 +13,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lists.component.css']
 })
 export class ListsComponent implements OnInit {
+  users: User[];
+  user: User = JSON.parse(localStorage.getItem('user'));
+  pagination: Pagination;
+  likesParam: string;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private alertify: AlertifyService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.data.subscribe((data) => {
+      this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
+    });
+
+    this.likesParam = 'Likers';
+  }
+
+  pageChanged(event: PageChangedEvent): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService
+      .getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, null, this.likesParam)
+      .subscribe(
+        (res: PaginatedResult<User[]>) => {
+          this.users = res.result;
+          this.pagination = res.pagination;
+        },
+        (error) => {
+          this.alertify.error(error);
+        }
+      );
   }
 
 }
