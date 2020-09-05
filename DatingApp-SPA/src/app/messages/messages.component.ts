e51802005a63f296bc61c2f690ed20d15947a614
Ali <aliii1984@outlook.com>
2020-09-05 14:07:12 +0430

    Message Service completed

--- DatingApp-SPA/src/app/messages/messages.component.ts
+++ DatingApp-SPA/src/app/messages/messages.component.ts
@@ -1,4 +1,10 @@
+import { AlertifyService } from './../_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from './../_services/auth.service';
+import { UserService } from 'src/app/_services/user.service';
+import { Pagination, PaginatedResult } from './../_models/pagination';
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../_models/message';
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +12,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+  messages: Message[];
+  pagination: Pagination;
+  messageContainer = 'Unread';
 
-  constructor() { }
+  constructor(private userService: UserService,
+              private authService: AuthService,
+              private route: ActivatedRoute,
+              private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.route.data.subscribe(
+      data => {
+        this.messages = data['messages'].result;
+        this.pagination = data['messages'].pagination;
+      }
+    );
   }
 
+  loadMesages(){
+    this.userService.getMessages(this.authService.decodedToken.nameid,
+        this.pagination.currentPage, this.pagination.itemsPerPage, this.messageContainer)
+        .subscribe(
+          (res: PaginatedResult<Message[]>) => {
+            this.messages = res.result;
+            this.pagination = res.pagination;
+          },
+          error => {
+            this.alertify.error(error);
+          });
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadMesages();
+  }
 }
