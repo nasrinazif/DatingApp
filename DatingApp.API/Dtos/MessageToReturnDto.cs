bdbcf73d5ec23fab0fde13330fd2581515dd2b3b
Ali <aliii1984@outlook.com>
2020-09-05 11:59:04 +0430

    minor

--- DatingApp.API/Dtos/MessageToReturnDto.cs
+++ DatingApp.API/Dtos/MessageToReturnDto.cs
@@ -10,12 +10,15 @@ namespace DatingApp.API.Dtos
     {
         public int Id { get; set; }
         public int SenderId { get; set; }
-        public User Sender { get; set; }
+        public string SenderKnownAs { get; set; }
+        public string SenderPhotoUrl { get; set; }
         public int RecipientId { get; set; }
-        public User Recipient { get; set; }
+        public string RecipientKnownAs { get; set; }
+        public string RecipientPhotoUrl { get; set; }
         public string Content { get; set; }
         public bool IsRead { get; set; }
         public DateTime? DateRead { get; set; }
         public DateTime MessageSent { get; set; }
+
     }
 }
